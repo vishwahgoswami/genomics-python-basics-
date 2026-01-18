7441c4416bf61f982dbdaee349723c4a17628be0
vishwahgoswami <vishwagoswami20041407@gmail.com>
2026-01-18 14:15:40 +0530

    compute gc percentage correctly

--- gc_content.py
+++ gc_content.py
@@ -1,13 +1,7 @@
-def gc_content(dna)
-dna=dna.upper()
-g=dna.count("G")
-c=dna.count("C")
-return (g+c)/ 
-len(dna)*100  
+>>>dna='atgcatctcgatcgtacgtacgtacgtcgctagctgctagctacgtcgcgatcgtacgatcgatttatatataatatattatgttggtgtcggcgcgtctctcgagag'
+no_c=dna.count(c)
+no_g=dna.count(g)
+dna_length=len(dna)
+gc_percentage=(no_c+ no_g)*100/dna_length
+print(gc_percentage)
 
-
-sequence =
-"ATGCGATACGCTTGA"
-gc=
-gc_content(sequence)
-print(f"gc content : {gc:.2f}%")
